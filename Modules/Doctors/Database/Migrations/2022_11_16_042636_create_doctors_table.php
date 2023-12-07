9ab1016bff0408aced84481bf3bdefa951a0be98
jeromwork <v0vy8l8fcygtmy6mn>
2023-12-07 14:58:16 +0300

    j_feat: saved content in legacy format

--- Modules/Doctors/Database/Migrations/2022_11_16_042636_create_doctors_table.php
+++ Modules/Doctors/Database/Migrations/2022_11_16_042636_create_doctors_table.php
@@ -60,7 +60,8 @@ return new class extends Migration
             $table->tinyInteger('is_analyze')->default(0);
             $table->tinyInteger('off')->default(0);
             $table->text('research')->nullable();
-            $table->text('diplomsCache')->nullable();
+            $table->text('diploms_cache')->nullable();
+            $table->text('content_cache')->nullable();
             $table->string('quotes', 5000)->default('');
             $table->text('interviews')->nullable();
             $table->text('awards')->nullable();
