204685386dd3d2cbd520ae854b3fa15ca6baf630
sashape <apien@list.ru>
2024-02-22 15:39:45 +0300

    fix: download hd with audio

--- Modules/Content/Services/YoutubeDownloadOptionsService.php
+++ Modules/Content/Services/YoutubeDownloadOptionsService.php
@@ -1,25 +1,40 @@
 <?php
 
 namespace Modules\Content\Services;
+use Illuminate\Support\Facades\Log;
 use YouTube\DownloadOptions;
+use FFMpeg\FFMpeg;
+use FFMpeg\Format\Video\X264;
 class YoutubeDownloadOptionsService extends DownloadOptions
 {
     /**
      * Find fullHD YouTube download link
-     * @return string
+     * @return array
      */
-    public function getFullHdDownloadLink(): string
+    public function getFullHdDownloadLink(): array
     {
         $url = '';
         $bitrate = 0;
-        $quality = ['hd1080'=>'hd1080','tiny' => 'tiny', 'small' => 'small'];
+        $audioUrl = '';
+//        $audioBitrate = 0;
+//        $audio = false;
+        $quality = ['hd1080'=>'hd1080', 'hd720' => 'hd720','tiny' => 'tiny', 'small' => 'small'];
         $formats = parent::getAllFormats();
         foreach ($formats as $item) {
-            if ($item->quality == $quality['hd1080'] && $item->bitrate > $bitrate) {
+            if ($item->quality == $quality['hd720'] && $item->bitrate > $bitrate && $item->audioQuality) {
                 $url = $item->url;
                 $bitrate = $item->bitrate;
+//                Log::info('audio ' . (boolval($item->audioQuality)) ? 'true' : 'false');
+//                if(!$item->audioQuality || $item->audioQuality === ' ') {
+//                    $audio = true;
+//                }
             }
+//            if(strpos($item->mimeType, 'audio/mp4') && $item->bitrate > $audioBitrate && $audio) {
+//                $audioBitrate = $item->bitrate;
+//                $audioUrl = $item->url;
+//            }
         }
-        return $url;
+
+        return ['video' => $url, 'audio' => $audioUrl];
     }
 }
